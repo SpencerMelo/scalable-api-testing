45cc9a6796b148a2bc0851105ba2c49eaaa5da70
SpencerMelo <su.spencer@hotmail.com>
2019-09-19 20:55:22 -0300

    updating git ignore and removing unnecessary logging.

--- src/main/java/com/learning/Main.java
+++ src/main/java/com/learning/Main.java
@@ -1,12 +1,17 @@
 package com.learning;
 
 import org.junit.runner.JUnitCore;
+import java.util.logging.Logger;
+
 
 public class Main {
     public static void main(String[] args) {
+        String loggingPath = Main.class.getClassLoader().getResource("logging.properties").getFile();
+        System.setProperty("java.util.logging.config.file", loggingPath);
+
         JUnitCore jUnitCore = new JUnitCore();
         try {
-            jUnitCore.run(Class.forName("com.learning.suite." + args[0].trim()));
+            jUnitCore.run(Class.forName("com.learning.suite.MathSuite"));// + args[0].trim()));
         } catch (ClassNotFoundException e) {
             throw new IllegalArgumentException("Invalid suite name.");
         }
